5ca350e9a320e819f76e56953e7395ab66808747
Vincent van der Wal <info@vincentvanderwal.nl>
2025-11-17 11:03:38 +0100

    revert so far

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,6 @@
 import * as maplibregl from 'maplibre-gl';
 
-import type { Domain, Variable, DomainMetaData } from '../types';
-import { domainOptions } from './domains';
+import type { Domain, DomainMetaData, Variable } from '../types';
 
 const now = new Date();
 now.setHours(now.getHours() + 1, 0, 0, 0);
@@ -15,7 +14,7 @@ export function capitalize(s: string) {
 }
 
 export const closestDomainInterval = (time: Date, domain: Domain) => {
-	let newTime = new Date(time.getTime());
+	const newTime = new Date(time.getTime());
 	if (domain.time_interval > 1) {
 		if (time.getUTCHours() % domain.time_interval > 0) {
 			const closestUTCHour = time.getUTCHours() - (time.getUTCHours() % domain.time_interval);
@@ -45,33 +44,18 @@ export const closestModelRun = (domain: Domain, selectedTime: Date, latest?: Dom
 	return closestModelRun;
 };
 
-const iconGlobalDomain = domainOptions.find((dO) => dO.value === 'dwd_icon') as Domain;
-
 export const getOMUrl = (
-	time: Date = new Date(),
-	domain: Domain = iconGlobalDomain,
-	variable: Variable = { value: 'temperature_2m' },
-
-	mode?: 'dark' | 'bright' | undefined,
-	partial?: boolean | undefined,
-	modelRun?: Date | undefined,
+	time: Date,
+	mode: 'dark' | 'bright',
+	partial: boolean,
+	domain: Domain,
+	variable: Variable,
+	modelRun: Date,
 	paddedBounds?: maplibregl.LngLatBounds
 ) => {
-	if (!modelRun) {
-		modelRun = closestModelRun(domain, time);
-	}
-
-	let omUrl = `https://map-tiles.open-meteo.com/data_spatial/${domain.value}/${modelRun.getUTCFullYear()}/${pad(modelRun.getUTCMonth() + 1)}/${pad(modelRun.getUTCDate())}/${pad(modelRun.getUTCHours())}00Z/${time.getUTCFullYear()}-${pad(time.getUTCMonth() + 1)}-${pad(time.getUTCDate())}T${pad(time.getUTCHours())}00.om?variable=${variable.value}`;
-
-	if (mode && mode === 'dark') {
-		omUrl += `&dark=true`;
-	}
-	if (partial) {
-		omUrl += `&partial=true`;
-	}
 	if (paddedBounds) {
-		omUrl += `&bounds=${paddedBounds.getSouth()},${paddedBounds.getWest()},${paddedBounds.getNorth()},${paddedBounds.getEast()}`;
+		return `https://map-tiles.open-meteo.com/data_spatial/${domain.value}/${modelRun.getUTCFullYear()}/${pad(modelRun.getUTCMonth() + 1)}/${pad(modelRun.getUTCDate())}/${pad(modelRun.getUTCHours())}00Z/${time.getUTCFullYear()}-${pad(time.getUTCMonth() + 1)}-${pad(time.getUTCDate())}T${pad(time.getUTCHours())}00.om?dark=${mode === 'dark'}&variable=${variable.value}&partial=${partial}&bounds=${paddedBounds.getSouth()},${paddedBounds.getWest()},${paddedBounds.getNorth()},${paddedBounds.getEast()}`;
+	} else {
+		return `https://map-tiles.open-meteo.com/data_spatial/${domain.value}/${modelRun.getUTCFullYear()}/${pad(modelRun.getUTCMonth() + 1)}/${pad(modelRun.getUTCDate())}/${pad(modelRun.getUTCHours())}00Z/${time.getUTCFullYear()}-${pad(time.getUTCMonth() + 1)}-${pad(time.getUTCDate())}T${pad(time.getUTCHours())}00.om?dark=${mode === 'dark'}&variable=${variable.value}&partial=${partial}`;
 	}
-
-	return omUrl;
 };
