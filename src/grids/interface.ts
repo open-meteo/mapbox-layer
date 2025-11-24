52b2d21d8e6ac39d13f7f2cb4915314b0de70434
Vincent van der Wal <info@vincentvanderwal.nl>
2025-11-24 10:56:39 +0100

    rm type alteration grid from this pr

--- src/grids/interface.ts
+++ src/grids/interface.ts
@@ -4,7 +4,6 @@ export interface GridInterface {
 	getLinearInterpolatedValue(values: Float32Array, lat: number, lon: number): number;
 
 	getBounds(): Bounds;
-	getIndex?(lat: number, lon: number): number;
 	getCenter(): { lng: number; lat: number };
 	getCoveringRanges(south: number, west: number, north: number, east: number): DimensionRange[];
 }
